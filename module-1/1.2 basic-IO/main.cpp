cb8529aa4bbb39f61bd4db16d69fa22cb37a4c68
probir ghosh <probirghosh@outlook.com>
2023-01-17 05:49:05 +0600

    cpp

---
+++ module-1/1.2 basic-IO/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+// int main() {
+//     char str [100];
+//     cout << "Enter A String : " ;
+//     cin >> str;
+//     cout << "You Entered :" << str << endl;
+// }
+
+
+// input = Probir Ghosh
+// output = Probir
+
+// This is because the extraction operator >> works as scanf() in C and considers a space " " has a terminating character
+
